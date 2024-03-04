79397ca9bcd4b397451ba2a07841abb30128d80e
maualtamirano <158343122+maualtamirano@users.noreply.github.com>
2024-03-04 11:13:44 -0300

    Version 1

---
+++ script.js
@@ -0,0 +1,56 @@
+
+function encriptar() {
+
+    var texto = document.getElementById("areaEncriptar").value;
+    if (/[A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(texto)) {
+
+        return;
+    }
+
+    var txtEncriptado = texto.replace(/e/igm, "enter");
+    txtEncriptado = txtEncriptado.replace(/o/igm, "ober");
+    txtEncriptado = txtEncriptado.replace(/i/igm, "imes");
+    txtEncriptado = txtEncriptado.replace(/a/igm, "al");
+    txtEncriptado = txtEncriptado.replace(/u/igm, "ufat");
+
+    document.getElementById("areaDesencriptar").value = txtEncriptado;
+    document.getElementById("areaEncriptar").value = "";
+
+    document.getElementById("btnEncriptar").disabled = true;    
+    document.getElementById("btnDesencriptar").disabled = false;
+
+}
+
+
+function desencriptar() {
+    var texto = document.getElementById("areaEncriptar").value;
+    if (/[A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(texto)) {
+        return;
+    }
+
+    var txtDesencriptado = texto.replace(/enter/igm, "e");
+    txtDesencriptado = txtDesencriptado.replace(/ober/igm, "o");
+    txtDesencriptado = txtDesencriptado.replace(/imes/igm, "i");
+    txtDesencriptado = txtDesencriptado.replace(/al/igm, "a");
+    txtDesencriptado = txtDesencriptado.replace(/ufat/igm, "u");
+
+    document.getElementById("areaDesencriptar").value = txtDesencriptado;
+    document.getElementById("areaEncriptar").value = "";
+
+    document.getElementById("btnEncriptar").disabled = false;
+    document.getElementById("btnDesencriptar").disabled = true;
+}
+
+    function copiar() {
+        let textoDesencriptado = document.querySelector("#areaDesencriptar").value;
+        navigator.clipboard.writeText(textoDesencriptado)
+       .then(() => {
+        alert("Texto copiado exitosamente")
+           });
+           document.getElementById("areaDesencriptar").value = "";
+
+    }
+    
+    
+
+
